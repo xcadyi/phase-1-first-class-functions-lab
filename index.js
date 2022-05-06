42f1588fb8d1994924ca839acaf7de41c27e83e0
Selin Ayyildiz <xcadyix@gmail.com>
2022-05-06 16:21:37 -0400

    commit tree 11e066888c3750fb20b215a358fc5bcd744b2dbd

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers() {
+    const returnFirstTwoDrivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+        return ['Antonia', 'Nuru']
+    }
+
+
+function returnLastTwoDrivers() { 
+    const returnLastTwoDrivers=['Antonia', 'Nuru', 'Amari', 'Mo']
+    return ['Amari', 'Mo']
+}
+
+function createFareMultiplier() {
+    return function(x,y) {
+        return x * y 
+    }
+}
